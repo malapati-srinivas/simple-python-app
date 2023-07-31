729b92a3aa6d638fb13b7ff38ff75301192115a0
Srinivas Malapati <137642456+malapati-srinivas@users.noreply.github.com>
2023-07-31 13:24:05 -0400

    Update start_container.sh
    update

--- start_container.sh
+++ start_container.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+# !/bin/bash
 set -e
 
+# Pull the docker image from docker hub
 docker pull srinivas20111986/simple-python-flask-app
 
+# Run the docker image as container
 docker run -d -p 5000:5000 srinivas20111986/simple-python-flask-app
